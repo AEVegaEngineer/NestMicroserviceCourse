bae48ccbf0f4565157c7cbb2359b1d9500e7b102
Andres Vega <aevega1991@gmail.com>
2024-12-27 14:23:28 -0300

    feat: :sparkles: instalar class-transformer, class-validator, agregar validaciones en dto, crear servicio de creacion de productos en memoria

--- 01-reforzamiento/products/src/products/entities/product.entity.ts
+++ 01-reforzamiento/products/src/products/entities/product.entity.ts
@@ -1,1 +1,14 @@
-export class Product {}
+export class Product {
+  // public id: string;
+  // public name: string;
+  // public description?: string;
+  // public price: number;
+
+  constructor(
+    public id: string,
+    public name: string,
+    public price: number,
+    public description?: string,
+  ) {}
+  // TODO: updateWith
+}
--- 01-reforzamiento/products/src/products/products.service.ts
+++ 01-reforzamiento/products/src/products/products.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class ProductsService {
+  private products: Product[] = [];
+
   create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+    const { name, description, price } = createProductDto;
+    const newProduct = new Product(uuidv4(), name, price, description);
+    this.products.push(newProduct);
+    return this.products;
   }
 
   findAll() {
