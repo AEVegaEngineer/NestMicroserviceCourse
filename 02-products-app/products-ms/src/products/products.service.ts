1e5cf8e4d3c9dee898529b81203888b11f8de9c3
Andres Vega <aevega1991@gmail.com>
2024-12-29 14:24:53 -0300

    feat: :sparkles: S4L30 Paginar mediante prisma
    added a sqlite database, inserted data with a manual query, created product paginations on the findAll endpoint

--- 02-products-app/products-ms/src/products/products.service.ts
+++ 02-products-app/products-ms/src/products/products.service.ts
@@ -1,17 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaClient } from '@prisma/client';
+import { PaginationDto } from 'src/common/dto';
 
 @Injectable()
-export class ProductsService {
+export class ProductsService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger(ProductsService.name);
+  onModuleInit() {
+    this.$connect();
+    this.logger.log('database connected');
+  }
   create(createProductDto: CreateProductDto) {
-    return createProductDto;
+    // Puedo acceder directamente a this.product ya que estoy extendiendo la clase PrismaClient
+    // y dentro de prisma tengo la entidad Product
+    return this.product.create({ data: createProductDto });
   }
 
-  findAll() {
-    return `This action returns all products`;
-  }
+  async findAll(paginationDto: PaginationDto) {
+    const { page, limit } = paginationDto;
 
+    const totalProducts = await this.product.count();
+    const lastPage = Math.ceil(totalProducts / limit);
+
+    return {
+      data: await this.product.findMany({
+        take: limit,
+        skip: (page - 1) * limit,
+      }),
+      meta: {
+        totalProducts,
+        page,
+        lastPage,
+      },
+    };
+  }
   findOne(id: number) {
     return `This action returns a #${id} product`;
   }
