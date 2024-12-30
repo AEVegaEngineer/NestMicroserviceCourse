af33e0f734488d577d920b22c1ebba93cb134298
Andres Vega <aevega1991@gmail.com>
2024-12-30 03:01:58 -0300

    feat: :sparkles: S5L45 Paginar resultados y enviar payload
    Se instala class-transformer y class-validator para validar dtos y se inicializa su configuracion con validationPipe en el main

--- 02-products-app/client-gateway/src/products/products.controller.ts
+++ 02-products-app/client-gateway/src/products/products.controller.ts
@@ -7,8 +7,10 @@ import {
   Param,
   Delete,
   Inject,
+  Query,
 } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
+import { PaginationDto } from 'src/common/dto';
 import { PRODUCTS_SERVICE } from 'src/config';
 
 @Controller('products')
@@ -19,30 +21,38 @@ export class ProductsController {
   @Post()
   createProduct(@Body() createProductDto) {
     return 'Crea un producto';
+    // { cmd: 'create_product' }
   }
 
   @Get()
-  findAll() {
-    return this.productsService.send({ cmd: 'find_all_products' }, {});
+  findAll(@Query() paginationDto: PaginationDto) {
+    return this.productsService.send(
+      { cmd: 'find_all_products' },
+      { /*limit: 50 , page: 2*/ ...paginationDto },
+    );
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
+    //{ cmd: 'find_product_by_id' }
     return 'Obtiene un producto';
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateProductDto) {
+    // { cmd: 'update_product' }
     return 'Actualiza un producto';
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
+    //{ cmd: 'delete_product' }
     return 'Elimina un producto';
   }
 
   @Delete('soft/:id')
   softRemove(@Param('id') id: string) {
+    //{ cmd: 'soft_delete_product' }
     return 'Desactiva un producto';
   }
 }
