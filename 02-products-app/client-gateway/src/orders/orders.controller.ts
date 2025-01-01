cbf46c069d2796ee0cdf6cb2ae6f12988265e23f
Andres Vega <aevega1991@gmail.com>
2025-01-01 18:35:16 -0300

    feat: :sparkles: S6L60 Obtener orden por ID
    Obteniendo orden y propagando errores del MS al APIG

--- 02-products-app/client-gateway/src/orders/orders.controller.ts
+++ 02-products-app/client-gateway/src/orders/orders.controller.ts
@@ -7,10 +7,14 @@ import {
   Inject,
   Patch,
   ParseIntPipe,
+  ParseUUIDPipe,
+  Query,
 } from '@nestjs/common';
-import { ClientProxy, Payload } from '@nestjs/microservices';
+import { ClientProxy, Payload, RpcException } from '@nestjs/microservices';
 import { ORDERS_SERVICE } from 'src/config';
 import { CreateOrderDto } from './dto';
+import { PaginationDto } from 'src/common/dto';
+import { firstValueFrom } from 'rxjs';
 
 @Controller('orders')
 export class OrdersController {
@@ -24,18 +28,27 @@ export class OrdersController {
   }
 
   @Get()
-  findAll() {
-    return this.ordersService.send('findAllOrders', {});
+  findAll(@Query() paginationDto: PaginationDto) {
+    return this.ordersService.send('findAllOrders', {
+      /*limit: 50 , page: 2*/ ...paginationDto,
+    });
   }
 
   @Get(':id')
-  findOne(@Param('id', ParseIntPipe) id: number) {
-    return this.ordersService.send('findOneOrder', { id });
+  async findOne(@Param('id', ParseUUIDPipe) id: string) {
+    try {
+      const order = await firstValueFrom(
+        this.ordersService.send('findOneOrder', { id }),
+      );
+      return order;
+    } catch (error) {
+      throw new RpcException(error);
+    }
   }
 
   @Patch(':id')
   chageOrderStatus(
-    @Param('id', ParseIntPipe) id: number,
+    @Param('id', ParseUUIDPipe) id: string,
     @Body() changeOrderStatusDto,
   ) {
     return this.ordersService.send(
--- 02-products-app/orders-ms/src/orders/orders.service.ts
+++ 02-products-app/orders-ms/src/orders/orders.service.ts
@@ -1,6 +1,8 @@
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { PrismaClient } from '@prisma/client';
+import { PaginationDto } from 'src/common/dto';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class OrdersService extends PrismaClient implements OnModuleInit {
@@ -14,11 +16,39 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
     return this.order.create({ data: createOrderDto });
   }
 
-  findAll() {
-    return `This action returns all orders`;
+  async findAll(paginationDto: PaginationDto) {
+    const { page, limit } = paginationDto;
+
+    const totalOrders = await this.order.count();
+    const lastPage = Math.ceil(totalOrders / limit);
+
+    return {
+      data: await this.order.findMany({
+        take: limit,
+        skip: (page - 1) * limit,
+      }),
+      meta: {
+        totalOrders,
+        page,
+        lastPage,
+      },
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} order`;
+  findOne(id: string) {
+    const order = this.order
+      .findUniqueOrThrow({
+        where: {
+          id,
+        },
+      })
+      .catch((error) => {
+        this.logger.error(error);
+        throw new RpcException({
+          message: `Order with id #${id} not found`,
+          status: HttpStatus.NOT_FOUND,
+        });
+      });
+    return order;
   }
 }
